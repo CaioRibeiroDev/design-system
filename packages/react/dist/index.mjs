09693f4e85b26741d1f772fa17c05988a2015b22
Caio Anderson Ribeiro <69832122+CaioRibeiroDev@users.noreply.github.com>
2022-12-19 14:13:50 -0300

    :spearkles: Story: Button

--- packages/react/dist/index.mjs
+++ packages/react/dist/index.mjs
@@ -126,7 +126,22 @@ var Button = styled("button", {
   padding: "$2 $4",
   border: "0",
   fontWeight: "bold",
-  color: "$white"
+  color: "$white",
+  variants: {
+    size: {
+      small: {
+        fontSize: 14,
+        padding: "$2 $4"
+      },
+      big: {
+        fontSize: 16,
+        padding: "$3 $6"
+      }
+    }
+  },
+  defaultVariants: {
+    size: "small"
+  }
 });
 export {
   Button
--- packages/react/src/index.tsx
+++ packages/react/src/index.tsx
@@ -1,3 +1,4 @@
+import { ComponentProps } from 'react'
 import { styled } from './styles'
 
 export const Button = styled('button', {
@@ -8,4 +9,27 @@ export const Button = styled('button', {
   border: '0',
   fontWeight: 'bold',
   color: '$white',
+
+  variants: {
+    size: {
+      small: {
+        fontSize: 14,
+        padding: '$2 $4',
+      },
+      big: {
+        fontSize: 16,
+        padding: '$3 $6',
+      },
+    },
+  },
+
+  defaultVariants: {
+    size: 'small',
+  },
 })
+
+export type ButtonProps = ComponentProps<typeof Button>
+
+// export type ButtonProps = {
+//   size: 'small' | 'big'
+// }
