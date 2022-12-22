570750b6a6dca9c1573a518c5368d63d451572d7
Caio Anderson Ribeiro <69832122+CaioRibeiroDev@users.noreply.github.com>
2022-12-22 11:36:58 -0300

    :sparkles: Component TextInput

---
+++ packages/docs/src/stories/TextInput.stories.tsx
@@ -0,0 +1,40 @@
+import type { Meta, StoryObj } from '@storybook/react'
+import { Box, Text, TextInput, TextInputProps } from '@ignite-ui/react'
+
+export default {
+  title: 'Form/Text Input',
+  component: TextInput,
+  args: {},
+} as Meta<TextInputProps>
+
+export const Primary: StoryObj<TextInputProps> = {
+  args: {
+    placeholder: 'Enter',
+  },
+}
+
+export const WithPrefix: StoryObj<TextInputProps> = {
+  args: {
+    prefix: 'cal.com/',
+  },
+}
+
+export const Disabled: StoryObj<TextInputProps> = {
+  args: {
+    disabled: true,
+  },
+}
+
+export const WithLabel: StoryObj<TextInputProps> = {
+  args: {},
+  decorators: [
+    (Story) => {
+      return (
+        <Box as="label" css={{ display: 'flex', flexDirection: 'column' }}>
+          <Text size="sm">Email Address</Text>
+          {Story()}
+        </Box>
+      )
+    },
+  ],
+}
